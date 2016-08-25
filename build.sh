5f9b76bc51ab42e6bb4941965083d04aee751729
Vinay Malkani <vinay.malkani@mayfieldrobotics.com>
2016-08-25 12:11:49 -0700

    initial commit

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash 
+###
+## bashscript to build custom kernel for ubilinux that works on ubuntu
+###
+KERNEL_VERSION=4.4.13
+MINOR_VERSION=1
+BASE_HOST=http://ubilinux.org
+BASE_URL=ubilinux/pool/main/l/linux-ubilinux
+KERNEL_FILE=linux-ubilinux_${KERNEL_VERSION}.orig.tar.xz
+DSC_FILE=linux-ubilinux_${KERNEL_VERSION}-${MINOR_VERSION}.dsc
+PATCH_FILE=linux-ubilinux_${KERNEL_VERSION}-${MINOR_VERSION}.debian.tar.xz
+CONFIG_FILE=config-mayfield-${KERNEL_VERSION}
+KPKG_PACKAGE_URL=https://launchpad.net/ubuntu/+source/kernel-package/13.003/+build/5980712/+files/kernel-package_13.003_all.deb
+KPKG_PACKAGE=${KPKG_PACKAGE_URL##*/}
+
+
+#pull files
+wget -N $BASE_HOST/$BASE_URL/$KERNEL_FILE
+wget -N $BASE_HOST/$BASE_URL/$DSC_FILE
+wget -N $BASE_HOST/$BASE_URL/$PATCH_FILE 
+wget -N $KPG_PACKAGE
+
+apt-get -y update && apt-get -y upgrade
+apt-get -y build-dep linux-image-$(uname -r)
+dpkg -i KPKG_PACKAGE
+
+
+dpkg-source ${DSC_FILE}
+cp ${CONFIG_FILE} linux-ubilinux-${KERNEL_VERSION}/.config
+cd linux-ubilinux-${KERNEL_VERSION}
+fakeroot make-kpkg -j 2 --initrd --append-to-version=mayfield kernel-image kernel-headers
