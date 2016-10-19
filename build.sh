11471d7ed33ac87cad39fea5afaee7eea6862443
Vinay Malkani <vinay.malkani@mayfieldrobotics.com>
2016-10-19 10:43:38 -0700

    fix build.sh, add .gitignore

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 ## bashscript to build custom kernel for ubilinux that works on ubuntu
 ### AUTHOR: Vinay Malkani
 ## Copyright 2016 Mayfield Robotics
+mkdir -p work
 KERNEL_VERSION=4.4.13
 MINOR_VERSION=1
 BASE_HOST=http://ubilinux.org
@@ -16,6 +17,7 @@ KPKG_PACKAGE=${KPKG_PACKAGE_URL##*/}
 
 
 #pull files
+pushd  work
 wget -N $BASE_HOST/$BASE_URL/$KERNEL_FILE
 wget -N $BASE_HOST/$BASE_URL/$DSC_FILE
 wget -N $BASE_HOST/$BASE_URL/$PATCH_FILE 
@@ -24,10 +26,11 @@ wget -N "${KPKG_PACKAGE_URL}"
 
 sudo apt-get -y update && sudo apt-get -y upgrade
 sudo apt-get -y build-dep linux-image-$(uname -r)
-sudo dpkg -i KPKG_PACKAGE
+sudo dpkg -i ${KPKG_PACKAGE}
 
 
-dpkg-source ${DSC_FILE}
-cp ${CONFIG_FILE} linux-ubilinux-${KERNEL_VERSION}/.config
+dpkg-source -x ${DSC_FILE}
+cp -v ../${CONFIG_FILE} linux-ubilinux-${KERNEL_VERSION}/.config
 cd linux-ubilinux-${KERNEL_VERSION}
-fakeroot make-kpkg -j 2 --initrd --append-to-version="-mayfield" kernel-image kernel-headers
+fakeroot make-kpkg -j 3 --initrd --append-to-version="-mayfield" kernel-image kernel-headers
+popd
