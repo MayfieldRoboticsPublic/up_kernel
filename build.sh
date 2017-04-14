22e31139f3cf46f6a932e53ed568ed36e8be93c6
Paul Mathieu <paul@mayfieldrobotics.com>
2017-04-13 18:53:36 -0700

    Add UVC video patches
    Also apply them

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@
 ## bashscript to build custom kernel for ubilinux that works on ubuntu
 ### AUTHOR: Vinay Malkani
 ## Copyright 2016 Mayfield Robotics
-mkdir -p work
 KERNEL_VERSION=4.4.13
 MINOR_VERSION=1
 BASE_HOST=http://ubilinux.org
@@ -15,15 +14,20 @@ CONFIG_FILE=config-mayfield-${KERNEL_VERSION}
 KPKG_PACKAGE_URL=https://launchpad.net/ubuntu/+source/kernel-package/13.003/+build/5980712/+files/kernel-package_13.003_all.deb
 KPKG_PACKAGE=${KPKG_PACKAGE_URL##*/}
 
+# change that last number by the revision number
+DEB_REVISION=$KERNEL_VERSION-$MINOR_VERSION+may0
 
-#pull files
-pushd  work
+
+mkdir -p work
+pushd work
+
+# pull files
 wget -N $BASE_HOST/$BASE_URL/$KERNEL_FILE
 wget -N $BASE_HOST/$BASE_URL/$DSC_FILE
 wget -N $BASE_HOST/$BASE_URL/$PATCH_FILE 
 wget -N "${KPKG_PACKAGE_URL}"
 
-
+# is the upgrade really necessary?
 sudo apt-get -y update && sudo apt-get -y upgrade
 sudo apt-get -y build-dep linux-image-$(uname -r)
 sudo dpkg -i ${KPKG_PACKAGE}
@@ -32,5 +36,6 @@ sudo dpkg -i ${KPKG_PACKAGE}
 dpkg-source -x ${DSC_FILE}
 cp -v ../${CONFIG_FILE} linux-ubilinux-${KERNEL_VERSION}/.config
 cd linux-ubilinux-${KERNEL_VERSION}
-fakeroot make-kpkg -j 3 --initrd --append-to-version="-mayfield" kernel-image kernel-headers
+cat ../../patches/*.patch | patch -p1
+fakeroot make-kpkg -j 3 --initrd --append-to-version="-mayfield" --revision="$DEB_REVISION" kernel-image kernel-headers
 popd
